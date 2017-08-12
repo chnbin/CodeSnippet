f19eac18d6c14788478258cf739b29685e3110de
ChenPin Chang <chnbin@gmail.com>
2017-08-13 03:29:30 +0800

    Add Learning python in hard way

--- learningpythoninhardway/ex12.py
+++
@@ -1,6 +0,0 @@
-
-y = input("Name?")
-age = input("How old are you? ")
-height = input("How tail are you? ")
-weight = input("How much do you wight? ")
-print("So, you're %r years old, %r tail and %r heavy." % (age,height, weight))
