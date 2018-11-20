5c4059e6ca448d5ddca1cb5f44d1073a2957df1d
chnbin <chnbin@gmail.com>
2018-11-20 21:09:32 +0800

    class method example

--- OOP/employee.py
+++ OOP/employee.py
@@ -2,6 +2,7 @@ class Employee:
     raise_rate = 1.05
     number_of_employee = 0
 
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
@@ -14,6 +15,18 @@ class Employee:
 
     def raise_amount(self):
         self.pay = int(self.pay * Employee.raise_rate)
+    
+    def __str__(self):
+        return '{} {} {}'.format(self.first, self.last, self.pay)
+
+    @classmethod
+    def set_raise_rate(cls, rate):
+        cls.raise_rate = rate
+    
+    @classmethod
+    def get_Emp_form_string(cls, str):
+        first, last, pay = str.split('-')
+        return cls(first, last, pay)
 
 emp1 = Employee('Chen-Ping', 'Chang', 50000)
 emp2 = Employee('Asuka', 'Inaba', 80000)
@@ -40,3 +53,15 @@ print(emp1.number_of_employee)
 
 emp3 = Employee('Author', 'Morgan', 100000)
 print(emp1.number_of_employee)
+
+# class method example
+Employee.set_raise_rate(1.06)
+print(emp1.raise_rate)
+print(emp2.raise_rate)
+
+# Take class method as alternative constructor
+empString = 'John-Doe-10000'
+emp4 = Employee.get_Emp_form_string(empString)
+print(emp4)
+
+
