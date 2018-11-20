fc376a53b13a5891123a07c6f54c686a8ed142a6
chnbin <chnbin@gmail.com>
2018-11-20 21:42:47 +0800

    A simple inherit sample.

--- OOP/person.py
+++ OOP/person.py
@@ -2,5 +2,28 @@ class Person():
     def __init__(self, name):
         self.name = name
 
+    def __str__(self):
+        return self.name
+
+class MDPerson(Person):
+    def __init__(self, name):
+        self.name = 'Doctor ' + name
+
+    def __str__(self):
+        return 'MDPerson ' + self.name
+
+class Gunslinger(Person):
+    def __init__(self, name):
+        self.name = 'Gunslinger ' + name
+    
+    def __str__(self):
+        return self.name
+    
 man = Person('Chen-Ping')
-print(man.name)
+print(man)
+
+mdPerson = MDPerson('Asuka Inabe')
+print(mdPerson)
+
+gunslinger = Gunslinger('Author Morgan')
+print(gunslinger)
