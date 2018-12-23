d7588f771dff816e98faddb02215d86b8a9bf0ee
chnbin <chnbin@gmail.com>
2018-12-23 20:12:53 +0800

    Add find

---
+++ javascript/find.js
@@ -0,0 +1,13 @@
+const users = [
+  {name: 'chnbin', id: 1},
+  {name: 'chris', id: 2},
+  {name: 'Kohiruimaki', id: 3},
+  {name: 'chris', id: 4}
+];
+
+// Only find the first while condition is matched.
+let user = users.find((user) => {
+  return user.name === 'chris';
+});
+
+console.log(user);
