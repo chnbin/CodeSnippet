90a75e84bd19dae7076e9a50a2778a332b91782b
chnbin <chnbin@gmail.com>
2018-12-26 21:21:48 +0800

    ES6

--- javascript/Destructuring/exmaple.js
+++ javascript/Destructuring/exmaple.js
@@ -9,8 +9,13 @@ const savedFile = {
  * @param {savedFile} file
  * @return {string} Summary of file.
  */
+function fileSummary({name, extension, size}) {
+  return `The file ${name}.${extension} is of size ${size}.`;
+}
+
+/* ES5
 function fileSummary(file) {
   return `The file ${file.name}.${file.extension} is of size ${file.size}.`;
-}
+}*/
 
 console.log(fileSummary(savedFile));
