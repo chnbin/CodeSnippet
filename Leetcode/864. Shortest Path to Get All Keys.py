192e9d89718ff1424a965120ba63b04de0c11270
chnbin <chnbin@gmail.com>
2019-01-31 22:19:21 +0800

    Add comments

--- Leetcode/864. Shortest Path to Get All Keys.py
+++ Leetcode/864. Shortest Path to Get All Keys.py
@@ -7,15 +7,25 @@ class Solution(object):
         """
         M = len(grid)
         N = len(grid[0])
-        keys = 'abcdef'
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        canWalk = '@.abcdef'
-        startPoint = []
-        keyCount = 0
-        keyGot = 0
-        queue = []
-        visited = set()
 
+        # 所有的鑰匙
+        keys = 'abcdef'
+
+        # 可以走的地板，會隨著獲得新的鑰匙擴充，例如拿到a鑰匙，字串會變成'@.abcdefA'
+        canWalk = '@.abcdef'
+
+        # 地圖需要拿的鑰匙數量，是結束條件
+        keyCount = 0
+
+        # 紀錄已經得到的鑰匙數量
+        keyGot = 0
+
+        queue = []
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        visited = set()
+        startPoint = []
+
+        # 找出出發點，並且掃描整個迷宮共有幾隻鑰匙要拿
         for i in range(M):
             for j in range(N):
                 if grid[i][j] in '@':
@@ -24,6 +34,7 @@ class Solution(object):
                 if grid[i][j] in keys:
                     keyCount += 1
         
+        # Queue存放的依次是 座標x軸 座標y軸 目前步數 目前可以走哪些地板  目前已經獲得幾把鑰匙
         queue.append([startPoint[0], startPoint[1], 0, canWalk, keyGot])
         
         while queue:
@@ -33,10 +44,12 @@ class Solution(object):
                 x, y, step, canWalk, keyGot = queue.pop(0)
                 visited.add(str(x) + '-' + str(y) + '-' + canWalk)
 
+                # 如果這格地板是鑰匙，更新獲得鑰匙數量，並且擴充canWalk
                 if (grid[x][y] in keys and grid[x][y].upper() not in canWalk):
                     keyGot += 1
                     canWalk += grid[x][y].upper()
-
+                
+                # 如果得到全部鑰匙，就結束
                 if (keyGot == keyCount):
                     return step
 
@@ -45,6 +58,7 @@ class Solution(object):
                     newX = x + dx
                     newY = y + dy
                     newPos = str(newX) + '-' + str(newY) + '-' + canWalk
+
                     if (0 <= newX < M) and (0 <= newY < N) and (grid[newX][newY] in canWalk) and (newPos not in visited):
                         visited.add(newPos)
                         queue.append([newX, newY, step + 1, canWalk, keyGot])
