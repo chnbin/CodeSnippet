c904bad4445951384325ee8a69de7aa7014664bf
chnbin <chnbin@gmail.com>
2019-04-10 01:29:08 +0800

    Add leetcode 35 in java

---
+++ Leetcode/035. Search Insert Position.java
@@ -0,0 +1,9 @@
+class Solution {
+  public int searchInsert(int[] nums, int target) {
+      int length = nums.length;
+      for(int i = 0; i < length; i++) {
+          if (target <= nums[i]) { return i; }
+      }
+      return length;
+  }
+}
