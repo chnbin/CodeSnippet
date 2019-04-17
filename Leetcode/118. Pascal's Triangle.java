183e7af00089d8c841c1a26152c42306426fa13b
chnbin <chnbin@gmail.com>
2019-04-17 20:05:36 +0800

    Add Leetcode 118

---
+++ Leetcode/118. Pascal's Triangle.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+  public List<List<Integer>> generate(int numRows) {
+      List<List<Integer>> ans = new ArrayList<List<Integer>>();
+      if (numRows == 0) { return ans; }
+      int[] currlevel = {1};
+      ArrayList<Integer> level = new ArrayList<Integer>();
+      
+      for (int num: currlevel) { level.add(num);}
+      ans.add(new ArrayList<>(level));
+      
+      for (int i = 1; i < numRows; i++) {
+          int[] tmpLevel = new int[currlevel.length+1];
+          
+          for (int j = 0; j < tmpLevel.length; j++) {
+              if (j == 0) {
+                  tmpLevel[j] = currlevel[j];
+              } else if (j == tmpLevel.length - 1) {
+                  tmpLevel[j] = currlevel[currlevel.length-1];
+              } else {
+                  tmpLevel[j] = currlevel[j] + currlevel[j-1];
+              }
+          }
+          level.clear();
+          for (int num: tmpLevel) { level.add(num);}
+          ans.add(new ArrayList<>(level));
+          currlevel = tmpLevel;
+      }
+      return ans;
+  }
+}
