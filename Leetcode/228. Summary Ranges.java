f388a46589ebf0b5688c981d75ac0768112b0daf
chnbin <chnbin@gmail.com>
2019-04-17 21:20:10 +0800

    Add Leetcode 228

---
+++ Leetcode/228. Summary Ranges.java
@@ -0,0 +1,36 @@
+class Solution {
+  public List<String> summaryRanges(int[] nums) {
+      List<String> ans = new ArrayList<String>();
+      if (nums == null || nums.length == 0) { return ans; }
+      int start = 0;
+      int end = 0;
+      
+      for (int i = 0; i < nums.length; i++) {
+          if (i == 0) {
+              start = i;
+              end = i;
+          } else {
+              if (nums[i] - nums[i-1] == 1) {
+                  end = i;
+              } else {
+                  if (start == end) {
+                      ans.add("" + nums[end]);
+                  } else {
+                      ans.add(nums[start] + "->" + nums[end]);
+                  }
+                  start = i;
+                  end = i;
+              }
+          }
+          
+          if (i == nums.length - 1) {
+              if (start == end) {
+                  ans.add("" + nums[end]);
+              } else {
+                  ans.add(nums[start] + "->" + nums[end]);
+              } 
+          }
+      }
+      return ans;
+  }
+}
