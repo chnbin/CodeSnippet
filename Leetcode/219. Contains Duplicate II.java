32bdcf8f088edb5eb1015479aae7e5ac4fabd3a1
chnbin <chnbin@gmail.com>
2019-04-18 21:11:07 +0800

    Add Leetcode 219

---
+++ Leetcode/219. Contains Duplicate II.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+class Solution {
+  public boolean containsNearbyDuplicate(int[] nums, int k) {
+      HashMap<Integer, Integer> dict = new HashMap<Integer, Integer>();
+      
+      for (int i = 0; i < nums.length; i++) {
+          if (dict.containsKey(nums[i])) {
+              if (Math.abs(dict.get(nums[i]) - i) <= k) {
+                  return true;
+              }
+          }
+          dict.put(nums[i], i);
+      }
+      return false;
+  }
+}
