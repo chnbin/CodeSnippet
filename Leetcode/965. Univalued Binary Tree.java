b5fae9229f2206e425124b7ba6ccb997df6ec947
chnbin <chnbin@gmail.com>
2019-04-25 13:44:17 +0800

    Add Leetcode 965

---
+++ Leetcode/965. Univalued Binary Tree.java
@@ -0,0 +1,16 @@
+class Solution {
+  public boolean isUnivalTree(TreeNode root) {
+      if (root == null) { return true; }
+      if (root.left != null) {
+          if (root.val != root.left.val) {
+              return false;
+          }
+      }
+      if (root.right != null) {
+          if (root.val != root.right.val) {
+              return false;
+          }
+      }
+      return isUnivalTree(root.left) && isUnivalTree(root.right);
+  }
+}
