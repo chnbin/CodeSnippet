88b36ecf1eb37b293cde071d954a41f79c1e1013
chnbin <chnbin@gmail.com>
2019-04-25 19:51:33 +0800

    Add Leetcode 763

---
+++ Leetcode/763. Partition Labels.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+  public List<Integer> partitionLabels(String S) {
+      Map<Character, Integer> map = new HashMap<Character, Integer>();
+      LinkedList<Integer> ans = new LinkedList<Integer>();
+      
+      for (int i = 0; i < S.length(); i++) {
+          map.put(S.charAt(i), i);
+      }
+      
+      int start = 0;
+      int end = 0;
+      int anchor = 0;
+      
+      for (int i = 0; i < S.length(); i++) {
+          end = Math.max(end, map.get(S.charAt(i)));
+          if (i == end) {
+              ans.add(end-start+1);
+              start = end+1;
+          }
+      }
+      return ans;
+  }
+}
