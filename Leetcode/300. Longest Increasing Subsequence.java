df6ac2b7ccbae7665e03f922736432f3351e5493
chnbin <chnbin@gmail.com>
2019-05-02 19:39:54 +0800

    Add Leetcode 300

---
+++ Leetcode/300. Longest Increasing Subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+  // Dynamic programming
+  public int lengthOfLIS(int[] nums) {
+      if (nums == null || nums.length == 0) { return 0; }
+      if (nums.length == 1) { return 1; }
+      int[] dp = new int[nums.length];
+      int maxLen = Integer.MIN_VALUE;
+      
+      dp[0] = 1;
+      
+      for (int i = 1; i < nums.length; i++) {
+          dp[i] = 1;
+          for (int j = 0; j < i; j++) {
+              if (nums[i] > nums[j]) {
+                  dp[i] = Math.max(dp[i], dp[j]+1);
+              }
+          }
+          maxLen = Math.max(maxLen, dp[i]);
+      }
+      
+      return maxLen;
+  }
+}
