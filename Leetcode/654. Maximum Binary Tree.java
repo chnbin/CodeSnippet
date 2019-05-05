38cdfb0efb1298a052f2e67474871e1986d25aff
chnbin <chnbin@gmail.com>
2019-05-05 22:45:02 +0800

    Add Leetcode 654

---
+++ Leetcode/654. Maximum Binary Tree.java
@@ -0,0 +1,22 @@
+class Solution {
+  public TreeNode constructMaximumBinaryTree(int[] nums) {
+      if (nums == null || nums.length == 0) { return null; }
+      return helper(nums, 0, nums.length-1);
+  }
+  
+  private TreeNode helper(int[] nums, int start, int end) {
+      if (start > end) { return null; }
+      int maxIndex = start;
+      for (int i = start; i <= end; i++) {
+          if (nums[i] > nums[maxIndex]) {
+              maxIndex = i;
+          }
+      }
+      
+      TreeNode root = new TreeNode(nums[maxIndex]);
+      root.left = helper(nums, start, maxIndex - 1);
+      root.right = helper(nums, maxIndex + 1, end);
+      return root;
+  }
+}
+
