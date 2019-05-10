bf644bb2df718b64916396f316099d271e51510e
chnbin <chnbin@gmail.com>
2019-05-10 21:10:08 +0800

    Add recursive method

--- Leetcode/102. Binary Tree Level Order Traversal.java
+++ Leetcode/102. Binary Tree Level Order Traversal.java
@@ -21,4 +21,20 @@ class Solution {
       }
       return ans;
   }
+  // Recursive
+  public List<List<Integer>> levelOrder2(TreeNode root) {
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    if (root == null) { return res; }
+    helper(root, 1, res);
+    return res;
+  }
+  private void helper(TreeNode node, int level, List<List<Integer>> res) {
+    if (node == null) { return; }
+    if (res.size() < level) {
+        res.add(new ArrayList<Integer>());
+    }
+    res.get(level - 1).add(node.val);
+    helper(node.left, level + 1, res);
+    helper(node.right, level + 1, res);
+  }
 }
