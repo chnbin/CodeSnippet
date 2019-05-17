c6cef65cb9b99220574fcf495dc3cf4c11b0f0d7
chnbin <chnbin@gmail.com>
2019-05-17 18:57:38 +0800

    Add recusive version

--- Leetcode/107. Binary Tree Level Order Traversal II.java
+++ Leetcode/107. Binary Tree Level Order Traversal II.java
@@ -25,4 +25,24 @@ class Solution {
       
       return res;
   }
+  /**
+   * Recusive version
+   * @param root
+   * @return
+   */
+  public List<List<Integer>> levelOrderBottomRecusive(TreeNode root) {
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+    if (root == null) { return res; }
+    helper(root, 1, res);
+    return res;
+  }
+  private void helper(TreeNode node, int level, List<List<Integer>> res) {
+    if (node == null) { return; }
+    if (res.size() < level) {
+        res.add(0, new ArrayList<Integer>());
+    }
+    res.get(res.size() - 1 - (level - 1)).add(node.val);
+    helper(node.left, level + 1, res);
+    helper(node.right, level + 1, res);
+  }
 }
