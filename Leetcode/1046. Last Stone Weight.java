da8d2c77ec335e836318bd39c47cfa359e034185
chnbin <chnbin@gmail.com>
2019-05-19 12:27:28 +0800

    Add Max-heap answer

--- Leetcode/1046. Last Stone Weight.java
+++ Leetcode/1046. Last Stone Weight.java
@@ -1,8 +1,10 @@
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 class Solution {
   public int lastStoneWeight(int[] stones) {
-      if (stones == null || stones.length == 0) { return 0;}
+      // 1 <= stones.length <= 30
+      // if (stones == null || stones.length == 0) { return 0;}
       if (stones.length == 1) { return stones[0]; }
       PriorityQueue<Integer> heap = new PriorityQueue<Integer>();
       
@@ -17,5 +19,23 @@ class Solution {
           heap.offer(-1*remain);
       }
       return -1*heap.poll();
+  }
+
+  // Max-heap
+  public int lastStoneWeight2(int[] stones) {
+    if (stones.length == 1) { return stones[0]; }
+    PriorityQueue<Integer> heap = new PriorityQueue<Integer>(1000, Collections.reverseOrder());
+    
+    for (int stone: stones) {
+        heap.offer(stone);
+    }
+    
+    while (heap.size() != 1) {
+        int first = heap.poll();
+        int second = heap.poll();
+        int remain = Math.abs(first - second);
+        heap.offer(remain);
+    }
+    return heap.poll();
   }
 }
