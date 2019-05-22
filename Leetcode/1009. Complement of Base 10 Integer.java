a7aa44afc895687611e18bbdc6ea09d1274edc91
chnbin <chnbin@gmail.com>
2019-05-22 23:40:47 +0800

    Add comment

--- Leetcode/1009. Complement of Base 10 Integer.java
+++ Leetcode/1009. Complement of Base 10 Integer.java
@@ -1,5 +1,15 @@
 class Solution {
   public int bitwiseComplement(int N) {
+      /**
+       * Elegant way to change 0 to 1 and vice versa.
+       * 1. Subtraction
+       * i = 1 - i
+       * 
+       * 2. XOR the value with 1.
+       * 1 ^ 1 = 0;
+       * 0 ^ 0 = 1;
+       * Method 2 can flip true <--> false either way.
+       */
       if (N == 0) { return 1; }
       if (N == 1) { return 0; }
       int res = 0;
