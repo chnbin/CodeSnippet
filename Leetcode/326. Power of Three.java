d0397a5f083e350334b250a333ee8cdd91599ee4
chnbin <chnbin@gmail.com>
2019-05-28 21:30:15 +0800

    Add follow up

--- Leetcode/326. Power of Three.java
+++ Leetcode/326. Power of Three.java
@@ -10,4 +10,27 @@ class Solution {
       }
       return n == 1;
   }
+
+  public boolean isPowerOfThree2(int n) {
+    /**
+     *  Follow up: Could you do it without using any loop / recursion?
+     *  Senior high school math: change of base.
+     *  To check if log10(n)/Math.log10(3) is a postive integer, I think log(n)/log(3) words too.
+     *  And this code shows how to check if a number is a type of integer elegantly.
+     */
+    if (n <= 0) { return false;}
+    
+    double m = Math.log10(n)/Math.log10(3);
+    return m == (int)m;
+  }
+
+  public boolean isPowerOfThree3(int n) {
+    /**
+     * Follow up. A very smart way. Beacuse 3^19 < 2^31 < 3^20.
+     * So we check if 3^19 module n is zero.
+     */
+    if (n < 1) { return false;}
+    
+    return (Math.pow(3, 19) % n) == 0;
+  }
 }
