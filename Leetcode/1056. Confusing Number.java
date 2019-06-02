5f220f6226f1086c819eb72529daeb52d3eef2d1
chnbin <chnbin@gmail.com>
2019-06-02 17:23:10 +0800

    Add Leetcode 1056

---
+++ Leetcode/1056. Confusing Number.java
@@ -0,0 +1,24 @@
+class Solution {
+  public boolean confusingNumber(int N) {
+      Map<Integer, Integer> map = new HashMap<>();
+      map.put(0, 0);
+      map.put(1, 1);
+      map.put(6, 9);
+      map.put(8, 8);
+      map.put(9, 6);
+      int newN = 0;
+      int oldN = N;
+      while (N > 0) {
+          int digit = N % 10;
+          
+          if (!map.containsKey(digit)) {
+              return false;
+          }
+          
+          newN = newN * 10 + map.get(digit);
+          N /= 10;
+      }
+      
+      return (newN != oldN) ? true : false;
+  }
+}
