9802219ceb9bb7accf2a62a45e3915c565b2f4c7
chnbin <chnbin@gmail.com>
2019-06-06 00:17:03 +0800

    Add Leetcode 760

---
+++ Leetcode/760. Find Anagram Mappings.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+  public int[] anagramMappings(int[] A, int[] B) {
+      Map<Integer, Integer> map = new HashMap<>();
+      
+      for (int i = 0; i < B.length; i++) {
+          if (!map.containsKey(B[i])) {
+              map.put(B[i], 0);
+          }
+          map.put(B[i], i);
+      }
+      
+      int[] res = new int[A.length];
+      for (int i = 0; i < A.length; i++) {
+          res[i] = map.get(A[i]);
+      }
+      return res;
+  }
+}
