353c5ec273e1382723414f28dd4b2d30466ec723
chnbin <chnbin@gmail.com>
2019-06-20 20:53:05 +0800

    Add Leetcode 230

---
+++ Leetcode/230. Kth Smallest Element in a BST.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+
+class Solution {
+  public int kthSmallest(TreeNode root, int k) {
+      if (root == null) { return -1; }
+      LinkedList<TreeNode> stack = new LinkedList<>();
+      
+      while (true) {
+          while (root != null) {
+              stack.push(root);
+              root = root.left;
+          }
+          
+          TreeNode node = stack.pop();
+          if (node != null) {
+              k--;
+              if (k == 0) { return node.val; }
+              if (node.right != null) {
+                  root = node.right;
+              }
+          }
+      }
+  }
+}
