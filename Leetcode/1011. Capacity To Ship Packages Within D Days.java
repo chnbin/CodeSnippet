2d5b99809e9181136235f1bcffab9fcd4d79b656
chnbin <chnbin@gmail.com>
2019-06-24 21:56:09 +0800

    Add Leetcode 1001

---
+++ Leetcode/1011. Capacity To Ship Packages Within D Days.java
@@ -0,0 +1,51 @@
+class Solution {
+  public int shipWithinDays(int[] weights, int D) {
+      int left = maxOfArray(weights);
+      int right = sumOfArray(weights);
+      int res = 0;
+      
+      while (left <= right) {
+          int mid = (left + right) / 2;
+          if (getDDay(weights, mid) <= D) {
+              right = mid - 1;
+              res = mid;
+          } else {
+              left = mid + 1;
+          } 
+      }
+      /*for (int i = left; i < right+1;i++) {
+          int day = getDDay(weights, i);
+          System.out.println(i + ": " + day);
+      }*/
+      return res;
+  }
+  
+  private int maxOfArray(int[] nums) {
+      int res = 0;
+      for (int num: nums) {
+           res = Math.max(res, num);
+      }
+      return res;
+  }
+  
+  private int sumOfArray(int[] nums) {
+      int res = 0;
+      for (int num: nums) {
+          res += num;
+      }
+      return res;
+  }
+  
+  private int getDDay(int[] nums, int weight) {
+      int days = 1;
+      int currWeight = 0;
+      for (int num: nums) {
+          if (currWeight + num > weight) {
+              days += 1;
+              currWeight = 0;
+          }
+          currWeight += num;
+      }
+      return days;
+  }
+}
