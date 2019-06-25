f9d91da5174cee16fa5d571fb01c43543a6d209e
chnbin <chnbin@gmail.com>
2019-06-25 21:09:17 +0800

    Shorten code

--- Leetcode/872. Leaf-Similar Trees.java
+++ Leetcode/872. Leaf-Similar Trees.java
@@ -1,21 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
-
 class Solution {
   public boolean leafSimilar(TreeNode root1, TreeNode root2) {
       List<Integer> seq1 = new ArrayList<>();
       List<Integer> seq2 = new ArrayList<>();
       contructLeafSqu(seq1, root1);
       contructLeafSqu(seq2, root2);
-      if (seq1.size() != seq2.size()) { return false; }
       
-      for (int i = 0; i < seq1.size(); i++) {
-          if (seq1.get(i) != seq2.get(i)) {
-              return false;
-          }
-      }
-      
-      return true;
+      return seq1.equals(seq2);
   }
   
   private void contructLeafSqu(List<Integer> list, TreeNode node) {
