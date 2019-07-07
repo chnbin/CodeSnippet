f452e79011ef73ddfa9537865fe1764899283b8e
chnbin <chnbin@gmail.com>
2019-07-07 12:25:07 +0800

    Add leetcode 1108

---
+++ Leetcode/1108. Defanging an IP Address.java
@@ -0,0 +1,15 @@
+class Solution {
+  public String defangIPaddr(String address) {
+      StringBuilder res = new StringBuilder();
+      
+      for (char c: address.toCharArray()) {
+          if (c == '.') {
+              res.append("[.]");
+          } else {
+              res.append(c);
+          }
+      }
+      
+      return res.toString();
+  }
+}
