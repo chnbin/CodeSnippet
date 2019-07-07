f40fc114bc9507bb20ee2fe20b7f413b293e4d41
chnbin <chnbin@gmail.com>
2019-07-07 12:52:25 +0800

    Add Leetcode 669

---
+++ Leetcode/669. Trim a Binary Search Tree.java
@@ -0,0 +1,14 @@
+class Solution {
+  public TreeNode trimBST(TreeNode root, int L, int R) {
+      if (root == null) { return null; }
+      if (root.val < L) {
+          root = trimBST(root.right, L, R);
+      } else if (root.val > R) {
+          root = trimBST(root.left, L, R);
+      } else {
+          root.left = trimBST(root.left, L, R);
+          root.right = trimBST(root.right, L, R);
+      }
+      return root;
+  }
+}
