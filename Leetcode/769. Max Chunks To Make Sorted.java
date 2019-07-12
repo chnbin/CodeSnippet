1216a139461044fffa1a9d1d6043c4486adff73f
chnbin <chnbin@gmail.com>
2019-07-12 21:15:36 +0800

    Add Leetcode 769

--- Leetcode/769. Max Chunks To Make Sorted.java
+++ Leetcode/769. Max Chunks To Make Sorted.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Solution {
   public int maxChunksToSorted(int[] arr) {
       int res = 0;
@@ -11,4 +13,26 @@ class Solution {
       }
       return res;
   }
+  
+  public int maxChunksToSorted2(int[] arr) {
+    int res = 0;
+    int[] sortedArr = arr.clone();
+    Arrays.sort(sortedArr);
+    int[] diff = new int[arr.length];
+    
+    for (int i = 0; i < arr.length; i++) {
+        diff[i] = sortedArr[i] - arr[i];
+    }
+    
+    int sum = 0;
+    
+    for (int i = 0; i < diff.length; i++) {
+        sum += diff[i];
+        
+        if (sum == 0) {
+            res++;
+        }
+    }
+    return res;
+}
 }
