75c3a0ae78363a0beae40817e5076e8d8fa890e4
chnbin <chnbin@gmail.com>
2019-07-13 20:27:33 +0800

    Add Leetcode 121

---
+++ Leetcode/121. Best Time to Buy and Sell Stock.java
@@ -0,0 +1,18 @@
+class Solution {
+  public int maxProfit(int[] prices) {
+      if (prices == null || prices.length < 2) {
+          return 0;
+      }
+      int minPrice = prices[0];
+      int profit = 0;
+      
+      for (int i = 1; i < prices.length; i++) {
+          if (minPrice < prices[i]) {
+              profit = Math.max(profit, prices[i] - minPrice);
+          } else {
+              minPrice = prices[i];
+          }
+      }
+      return profit;
+  }
+}
