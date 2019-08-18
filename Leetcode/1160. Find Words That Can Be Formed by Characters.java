d8eba8ac03ceddd6ed0312b542ae5c2ea01a82d2
chnbin <chnbin@gmail.com>
2019-08-18 12:46:06 +0800

    Add Leetcode 1160

---
+++ Leetcode/1160. Find Words That Can Be Formed by Characters.java
@@ -0,0 +1,32 @@
+class Solution {
+  public int countCharacters(String[] words, String chars) {
+      int res = 0;
+      
+      int[] charCnt = new int[26];
+      
+      for (char c: chars.toCharArray()) {
+          charCnt[c - 'a']++;
+      }
+      
+      for (String word: words) {
+          int[] tmp = Arrays.copyOf(charCnt, charCnt.length);
+          if (isGood(word, tmp)) {
+              res += (word.length());
+          }
+      }
+      return res;
+  }
+      
+  private boolean isGood(String str, int[] charCnt) {
+      boolean res = true;
+      
+      for (char c: str.toCharArray()) {
+          if (charCnt[c - 'a'] == 0) {
+              return false;
+          } else {
+              charCnt[c - 'a']--;
+          }
+      }
+      return res;
+  }
+}
