7cb311cd328d1b3558f466213a760df68de026ed
chnbin <chnbin@gmail.com>
2019-09-07 15:40:41 +0800

    ADD ABC084 C

--- Atcoder/ABC084/Main.java
+++ Atcoder/ABC084/Main.java
@@ -12,6 +12,7 @@ public class Main {
     solveA();
     solveB();
     solveC();
+    solveD();
   }
   
 
@@ -42,6 +43,42 @@ public class Main {
     }
     System.out.println("Yes");
     System.exit(0);
+  }
+
+  private static void solveC() {
+    FastReader sc = new FastReader();
+    int n = sc.nextInt();
+    int[][] t = new int[n-1][3];
+    
+    for (int i = 0; i < n - 1; i++) {
+      t[i][0] = sc.nextInt();
+      t[i][1] = sc.nextInt();
+      t[i][2] = sc.nextInt();
+    }
+    
+    for (int i = 0; i < n - 1; i++) {
+      int res = 0;
+      int c = t[i][0];
+      int s = t[i][1];
+      long arrive = c + s;
+      
+      for (int j = i + 1; j < n - 1; j++) {
+        int readyToGo = t[j][1];
+        
+        if (arrive < readyToGo) {
+          arrive = readyToGo + t[j][0];
+        } else {
+          if ((arrive - t[j][1]) % t[j][2] == 0) {
+            arrive += t[j][0]; 
+          } else {
+            arrive = arrive + (t[j][2]) - ((arrive - t[j][1]) % t[j][2]) + t[j][0];
+          }
+        }
+      }
+      
+      System.out.println(arrive);
+    }
+    System.out.println(0);
   }
 
   private static void solveD() {
