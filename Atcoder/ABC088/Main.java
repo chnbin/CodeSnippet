7424ff9cc755b4483c83e81a11a212e444d4b7fd
chnbin <chnbin@gmail.com>
2019-09-10 21:58:44 +0800

    ADD ABC088 D

--- Atcoder/ABC088/Main.java
+++ Atcoder/ABC088/Main.java
@@ -1,16 +1,18 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-  final static int MOD = 10007;
 
   public static void main(String[] args) {
     solveA();
     solveB();
     solveC();
+    solveD();
   }
   
   private static void solveA() {
@@ -101,6 +103,62 @@ public class Main {
     System.out.println("Yes");
   }
 
+  private static void solveD() {
+    FastReader sc = new FastReader();
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+    
+    char[][] maze = new char[n][m];
+    boolean[][] visited = new boolean[n][m];
+    int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    int white = 0;
+    
+    for (int i = 0; i < n; i++) {
+      String s = sc.next();
+      for (int j = 0; j < m; j++) {
+        char c = s.charAt(j);
+        maze[i][j] = c;
+        if (c == '.') { white++;}
+      }
+    }
+    
+    int step = 1;
+    Queue<Point> q = new LinkedList<>();
+    q.offer(new Point(0,0,1));
+    
+    while (!q.isEmpty()) {
+      int size = q.size();
+      // System.out.println(size);
+      for (int i = 0; i < size; i++) {
+        Point p = q.poll();
+        int x = p.x;
+        int y = p.y;
+        
+        visited[x][y] = true;
+        
+        if (x == (n - 1) && y == (m - 1)) {
+          System.out.println(white - step);
+          System.exit(0);
+        }
+        
+        for (int[] dir: dirs) {
+          int dx = x + dir[0];
+          int dy = y + dir[1];
+          
+          if ((0 <= dx) && (dx <n) && 
+            (0 <= dy) && (dy < m) && 
+            maze[dx][dy] == '.' && 
+            !visited[dx][dy]) {
+            visited[dx][dy] = true;
+            q.offer(new Point(dx, dy, step));
+          }
+        }
+      }
+      step++;
+    }
+    System.out.println(-1);
+  }
+
   static class FastReader {
     BufferedReader br;
     StringTokenizer st;
@@ -141,5 +199,15 @@ public class Main {
         }
         return str;
       }
+  }
+}
+
+class Point {
+  public int x;
+  public int y;
+  public int step;
+  public Point(int x, int y, int step) {
+    this.x = x;
+    this.y = y;
   }
 }
