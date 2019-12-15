f321386ff3abbcb223b89ed4277da2452a949d78
chnbin <chnbin@gmail.com>
2019-12-15 15:03:59 +0800

    Add Leetcode 1290

---
+++ Leetcode/1290. Convert Binary Number in a Linked List to Integer.java
@@ -0,0 +1,13 @@
+class Solution {
+  public int getDecimalValue(ListNode head) {
+      int res = 0;
+      
+      while (head != null) {
+          res *= 2;
+          res += head.val;
+          head = head.next;
+      }
+      
+      return res;
+  }
+}
