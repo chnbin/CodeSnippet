1121790306a35c666cbb121f359cacf1037dc221
chnbin <chnbin@gmail.com>
2019-12-30 19:43:49 +0800

    Add Leetcode 454

---
+++ Leetcode/454. 4Sum II.java
@@ -0,0 +1,23 @@
+class Solution {
+  public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+      Map<Integer, Integer> map = new HashMap<>();
+      int res = 0;
+      
+      for (int i = 0; i < A.length; i++) {
+          for (int j = 0; j < B.length; j++) {
+              map.put(A[i] + B[j], map.getOrDefault(A[i] + B[j], 0) + 1);
+          }
+      }
+      
+      for (int i = 0; i < C.length; i++) {
+          for (int j = 0; j < D.length; j++) {
+              int target = -1 * (C[i] + D[j]);
+              if (map.containsKey(target)) {
+                  res += map.get(target);
+              }
+          }
+      }
+      
+      return res;
+  }
+}
