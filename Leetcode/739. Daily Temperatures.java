4779a5e9ef4cfeab5ed8979e274e61e6a5f8d0f2
chnbin <chnbin@gmail.com>
2019-12-31 19:32:53 +0800

    Add Leetcode 739

---
+++ Leetcode/739. Daily Temperatures.java
@@ -0,0 +1,27 @@
+class Solution {
+  public int[] dailyTemperatures(int[] T) {
+      int n = T.length;
+      int[] res = new int[n];
+      LinkedList<DayInfo> stack = new LinkedList<>();
+      
+      for (int i = 0; i < n; i++) {
+          while (!stack.isEmpty() && T[i] > stack.peek().temperature) {
+              DayInfo dayInfo = stack.pop();
+              res[dayInfo.idx] = i - dayInfo.idx;
+          }
+          
+          stack.push(new DayInfo(T[i], i));
+      }
+      
+      return res;
+  }
+  
+  private class DayInfo {
+      public int temperature;
+      public int idx;
+      DayInfo(int temperature, int idx) {
+          this.temperature = temperature;
+          this.idx = idx;
+      }
+  }
+}
