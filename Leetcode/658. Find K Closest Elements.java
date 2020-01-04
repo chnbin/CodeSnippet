ded59fa6da810dd5d26786f630b5f3fb0d73cfe5
chnbin <chnbin@gmail.com>
2020-01-04 23:32:08 +0800

    Use two pointers to slove this problem

--- Leetcode/658. Find K Closest Elements.java
+++ Leetcode/658. Find K Closest Elements.java
@@ -1,35 +1,32 @@
 class Solution {
   public List<Integer> findClosestElements(int[] arr, int k, int x) {
-      PriorityQueue<Element> pq = new PriorityQueue<>((a, b) -> {
-          if (a.dist == b.dist) {
-              return a.pos - b.pos;
-          }
-          
-          return a.dist - b.dist;
-      });
-      
-      for (int num: arr) {
-          pq.offer(new Element(num, Math.abs(num - x)));
-      }
-      
+      /*
+        這道題給我們了一個數組，還有兩個變量k和x。讓找數組中離x最近的k個元素。而且說明了陣列已經排序。
+        故兩個數字距離x相等的話，取較小的那個。
+        從給定的例子可以分析出x不一定是數組中的數字，由於陣列有序，所以最後返回的k個元素也一定是有序的，
+        本題就是就是返回了原陣列的一個長度為k的子陣列。
+        轉化一下，實際上相當於在長度為n的陣列中去掉 n-k 個數字，而且去掉的順序肯定是從兩頭執行，
+        因為距離x最遠的數字肯定在首尾出現。
+        那麽問題就變的明朗了，每次比較首尾兩個數字跟x的距離，然後將距離大的那個數字刪除，直到陣列長度為k為止。
+      */
+      int left = 0;
+      int right = arr.length - 1;
       List<Integer> res = new ArrayList<>();
+      k = arr.length - k;
       
       while (k > 0) {
-          res.add(0, pq.poll().pos);
+          if (Math.abs(arr[left] - x) <= Math.abs(arr[right] - x)) {
+              right--;
+          } else {
+              left++;
+          }
           k--;
       }
       
-      Collections.sort(res);
+      for (int i = left; i <= right; i++) {
+          res.add(arr[i]);
+      }
       
       return res;
-  }
-}
-
-class Element {
-  public int pos;
-  public int dist;
-  public Element(int pos, int dist) {
-      this.pos = pos;
-      this.dist = dist;
   }
 }
