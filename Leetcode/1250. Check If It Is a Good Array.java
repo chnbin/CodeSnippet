1956668891a093f831bcdf5928e68afbeae46741
chnbin <chnbin@gmail.com>
2020-01-08 21:00:05 +0800

    Add comment to Leetcode 1250

--- Leetcode/1250. Check If It Is a Good Array.java
+++ Leetcode/1250. Check If It Is a Good Array.java
@@ -1,4 +1,12 @@
 class Solution {
+  /*
+    對於隨意選出的兩個整數a與b，要讓ax+by = 1，其最大公因數應該為1。
+    使用數學的語言來講：方程式 ax+by=1 有整數解若且唯若整數a和b互質
+    而對於隨意選出的n個整數a1, a2, ..., an要讓a1x + a2y + ... + anz = 1，
+    就是a1, a2, ..., an的最大公因數為1。
+    這不用一對一對去算，只要一對為1就可以，因為：
+    GCD(a1, a2, ..., an) = GCD(GCD(GCD(GCD(a1, a2),a3),a4)....an)
+  */
   public boolean isGoodArray(int[] nums) {
       if (nums.length == 1) {
           return (nums[0] == 1) ? true : false;
