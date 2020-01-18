288025719541cc86629b62144f76e96c250a8313
chnbin <chnbin@gmail.com>
2020-01-18 18:48:59 +0800

    Add Leetcode 1265 - Liner time and O(n) space

---
+++ Leetcode/1265. Print Immutable Linked List in Reverse.java
@@ -0,0 +1,24 @@
+/**
+ * // This is the ImmutableListNode's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * interface ImmutableListNode {
+ *     public void printValue(); // print the value of this node.
+ *     public ImmutableListNode getNext(); // return the next node.
+ * };
+ */
+
+class Solution {
+  public void printLinkedListInReverse(ImmutableListNode head) {
+      LinkedList<ImmutableListNode> stack = new LinkedList<>();
+      
+      while (head != null) {
+          stack.push(head);
+          head = head.getNext();
+      }
+      
+      while (!stack.isEmpty()) {
+          ImmutableListNode node = stack.pop();
+          node.printValue();
+      }
+  }
+}
