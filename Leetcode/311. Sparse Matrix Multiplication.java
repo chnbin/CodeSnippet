e04f26967bc87efb2beea29130232b9149393ae3
chnbin <chnbin@gmail.com>
2020-01-21 20:20:01 +0800

    Add Leetcode 311

---
+++ Leetcode/311. Sparse Matrix Multiplication.java
@@ -0,0 +1,19 @@
+class Solution {
+  public int[][] multiply(int[][] A, int[][] B) {
+      int[][] res = new int[A.length][B[0].length];
+      
+      for (int i = 0; i < A.length; i++) {
+          for (int j = 0; j < A[0].length; j++) {
+              if (A[i][j] != 0) {
+                  for (int k = 0; k < B[0].length; k++) {
+                      if (B[j][k] != 0) {
+                          res[i][k] += (A[i][j] * B[j][k]);
+                      }
+                  }
+              }
+          }
+      }
+      
+      return res;
+  }
+}
