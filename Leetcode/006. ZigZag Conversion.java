3befdfd9575e349f4fab5750b548f0e4ab4cfc72
Chen-Pin Chang <chnbin@gmail.com>
2020-04-17 21:22:25 +0800

    Update 006. ZigZag Conversion.java
    Solve this problem with StringBuilder.

--- Leetcode/006. ZigZag Conversion.java
+++ Leetcode/006. ZigZag Conversion.java
@@ -1,34 +1,31 @@
 class Solution {
-  public String convert(String s, int numRows) {
-      if (s == null || s.length() == 0) {
-          return "";
-      }
-      
-      if (numRows == 1) {
-          return s;
-      }
-      
-      String[] groups = new String[numRows];
-      String ans = "";
-      int pos = 0;
-      int direction = -1;
-      
-      for (int i = 0; i < numRows; i++) {
-          groups[i] = "";
-      }
-      
-      for (int i = 0; i < s.length(); i++) {
-          groups[pos] += s.charAt(i);
-          
-          if (pos == 0 || pos == (numRows - 1)) {
-              direction *= -1;
-          }
-          pos += direction;
-      }
-      
-      for (String group: groups) {
-          ans += group;
-      }
-      return ans;
-  }
+    public String convert(String s, int numRows) {
+        if (s == null || s.length() == 0) { return s; }
+        if (numRows == 1) { return s;}
+        
+        StringBuilder[] sb = new StringBuilder[numRows];
+        StringBuilder res = new StringBuilder();
+        int pos = 0;
+        int direction = -1;
+
+        for (int i = 0; i < sb.length; i++) {
+            sb[i] = new StringBuilder("");
+        }
+        
+        for (char c: s.toCharArray()) {
+            sb[pos].append(c);
+
+            if (pos == 0 || pos == numRows - 1) {
+                direction *= -1;
+            }
+
+            pos += direction;
+        }
+
+        for (StringBuilder str: sb) {
+            res.append(str.toString());
+        }
+
+        return res.toString();
+    }
 }
