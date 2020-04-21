43b0c45d6f812f29b1558a80a440a6e34419dfcf
chnbin <chnbin@gmail.com>
2020-04-21 19:56:59 +0800

    Create Counting Elements.java
    30 day leetcoding challenge Day 7

---
+++ Leetcode/tmp/Counting Elements.java
@@ -0,0 +1,28 @@
+package tmp;
+/*
+  30 day leetcoding challenge
+  Day 7. Counting Elements
+  https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/528/week-1/3289/
+
+  Problem list does not include this problem so add it.
+  Time Complexity : O(n), where n is the length of arr.
+ */
+class Solution {
+  public int countElements(int[] arr) {
+      if (arr.length == 1) { return 0;}
+      int res = 0;
+      int[] table = new int[1001];
+      
+      for (int num: arr) {
+          table[num]++;
+      }
+      
+      for (int i = 0; i < table.length - 1; i++) {
+          if (table[i] > 0 && table[i+1] > 0) {
+              res += table[i];
+          }
+      }
+      
+      return res;
+  }
+}
