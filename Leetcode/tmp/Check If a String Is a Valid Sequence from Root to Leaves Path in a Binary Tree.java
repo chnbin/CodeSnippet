d2c56d0ee78e00dce4d147ce786180960f93e367
chnbin <chnbin@gmail.com>
2020-04-30 20:18:28 +0800

    Update Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree.java
    Add BFS solution.

--- Leetcode/tmp/Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree.java
+++ Leetcode/tmp/Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree.java
@@ -11,6 +11,9 @@ class Solution {
       return isValidSequenceHelper(root, arr, 0);
   }
   
+  // DFS solution
+  // Time complexity: O(n)
+  // Space complexity: O(n), where n is the numbers of tree
   private boolean isValidSequenceHelper(TreeNode node, int[] arr, int pos) {
       if (pos > arr.length - 1) { return false; }
       
@@ -32,5 +35,40 @@ class Solution {
           return left || right;
       }
       return false;
+  }
+
+  // BFS solution
+  // Time complexity: O(n)
+  // Space complexity: O(n), where n is the numbers of tree
+  public boolean isValidSequenceBFS(TreeNode root, int[] arr) {
+    Queue<TreeNode> queue = new LinkedList<>();
+    int pos = 0;
+    queue.offer(root);
+    
+    while (!queue.isEmpty()) {
+        int size = queue.size();
+        
+        for (int i = 0; i < size; i++) {
+            TreeNode node = queue.poll();
+            
+            if (pos > arr.length - 1) {
+                return false;
+            }
+            
+            if (node.val != arr[pos]) {
+                continue;
+            }
+            
+            if (node.left == null && node.right == null && (pos == arr.length - 1)) {
+                return true;
+            }
+            
+            if (node.left != null) { queue.offer(node.left); }
+            if (node.right != null) { queue.offer(node.right); }
+        }
+        pos++;
+    }
+    
+    return false;
   }
 }
