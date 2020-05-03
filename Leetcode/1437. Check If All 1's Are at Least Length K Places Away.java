5056bab1554637ebbc89b2a8f0b063d071ef7a61
Chen-Pin Chang <chnbin@gmail.com>
2020-05-03 12:26:18 +0800

    Create 1437. Check If All 1's Are at Least Length K Places Away.java

---
+++ Leetcode/1437. Check If All 1's Are at Least Length K Places Away.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+        int idx = -1;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                if (idx != -1 && (i - idx) <= k) {
+                    return false;
+                }
+                idx = i;
+            }
+        }
+        
+        return true;
+    }
+}
