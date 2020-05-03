0721650e942a8cb363b1f6acd255b75ca733aa56
Chen-Pin Chang <chnbin@gmail.com>
2020-05-03 16:01:36 +0800

    Update 383. Ransom Note.java
    Use an interge array to count the number of each letters.
    This will be fast than hashmap.

--- Leetcode/383. Ransom Note.java
+++ Leetcode/383. Ransom Note.java
@@ -1,23 +1,24 @@
-import java.util.HashMap;
+/*
+  Use an interge array to count the number of each letters.
+  This will be fast than hashmap.
+*/
 
 class Solution {
-  public boolean canConstruct(String ransomNote, String magazine) {
-      HashMap<Character, Integer> chars = new HashMap<Character, Integer>();
-      
-      for (char c: magazine.toCharArray()) {
-          if (!chars.containsKey(c)) {
-              chars.put(c, 0);
-          }
-          chars.put(c, chars.get(c) + 1);
-      }
-
-      for (char c: ransomNote.toCharArray()) {
-          if (!chars.containsKey(c) || chars.get(c) == 0) {
-              return false;
-          }
-          chars.put(c, chars.get(c) - 1);
-      }
-      
-      return true;
-  }
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] letters = new int[26];
+        
+        for (char c: magazine.toCharArray()) {
+            letters[c - 'a']++;
+        }
+        
+        for (char c: ransomNote.toCharArray()) {
+            if (letters[c - 'a'] == 0) {
+                return false;
+            }
+            letters[c - 'a']--;
+        }
+        
+        
+        return true;
+    }
 }
