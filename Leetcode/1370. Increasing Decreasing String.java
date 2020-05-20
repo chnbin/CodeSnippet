b748c83a5554455a22ba92cf1b22c6fdde0329f0
chnbin <chnbin@gmail.com>
2020-05-20 22:23:37 +0800

    Create 1370. Increasing decreasing string

---
+++ Leetcode/1370. Increasing Decreasing String.java
@@ -0,0 +1,43 @@
+class Solution {
+  /*
+    Time comlexity: O(n) where n is the length of s.
+    First, we count the frequency of each character.
+    Step 1: scan frequency from 'a' to 'z'. If frequency of character is bigger than zero, append this character to answer.
+    Step 2: scan frequency from 'z' to 'a'. If frequency of character is bigger than zero, append this character to answer.
+    Do step1 and step2 repeatly until we use all characters.
+   */
+  public String sortString(String s) {
+      String res = "";
+      int[] tables = new int[26];
+      int n = s.length();
+      
+      for (char c: s.toCharArray()) {
+          tables[c - 'a']++;
+      }
+      
+      int direction = 1;
+      
+      while (n > 0) {
+          if (direction == 1) {
+              for (int i = 0; i < 26; i++) {
+                  if (tables[i] > 0) {
+                      tables[i]--;
+                      res += (char)(i + 'a');
+                      n--;
+                  }
+              }
+          } else {
+              for (int i = 25; i >= 0; i--) {
+                  if (tables[i] > 0) {
+                      tables[i]--;
+                      res += (char)(i + 'a');
+                      n--;
+                  }
+              }
+          }
+          direction *= -1;
+      }
+      
+      return res;
+  }
+}
