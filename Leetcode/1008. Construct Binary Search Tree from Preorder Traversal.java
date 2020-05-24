9fffd5c2de71c06175bc975eeea612f018a74ec7
chnbin <chnbin@gmail.com>
2020-05-24 16:31:28 +0800

    Update 1008. Construct Binary Search Tree from Preorder Traversal
    Add comment and complexity

--- Leetcode/1008. Construct Binary Search Tree from Preorder Traversal.java
+++ Leetcode/1008. Construct Binary Search Tree from Preorder Traversal.java
@@ -1,30 +1,39 @@
 class Solution {
   public TreeNode bstFromPreorder(int[] preorder) {
+      /*
+          The first element of the preorder traverse is root.
+          Step 1. Choose the first element as a binary search tree's root.
+          Step 2. By definition, insert remain elements into this tree from the root.
+          Time complexity: O(NlogN) where N is the number of tree nodes.
+                           Insert a node into BST is O(logN) and we need to insert N nodes.
+          Space complexity: O(1).
+      */
       TreeNode root = new TreeNode(preorder[0]);
       
       for (int i = 1; i < preorder.length; i++) {
-          TreeNode tmp = root;
+          TreeNode tmpRoot = root;
           
-          while (tmp != null) {
-              if (preorder[i] < tmp.val) {
-                  // To left
-                  if (tmp.left == null) {
-                      tmp.left = new TreeNode(preorder[i]);
+          while (tmpRoot != null) {
+              if (tmpRoot.val > preorder[i]) {
+                  // add this node to left if tmpRoot is leaf.
+                  if (tmpRoot.left == null) {
+                      tmpRoot.left = new TreeNode(preorder[i]);
                       break;
                   } else {
-                      tmp = tmp.left;
-                  }
+                      tmpRoot = tmpRoot.left;    
+                  }                    
               } else {
-                  // To right
-                  if (tmp.right == null) {
-                      tmp.right = new TreeNode(preorder[i]);
+                  // add this node to right if tmpRoot is leaf.
+                  if (tmpRoot.right == null) {
+                      tmpRoot.right = new TreeNode(preorder[i]);
                       break;
                   } else {
-                      tmp = tmp.right;
-                  }
+                      tmpRoot = tmpRoot.right;    
+                  }                    
               }
           }
       }
+      
       return root;
   }
 }
