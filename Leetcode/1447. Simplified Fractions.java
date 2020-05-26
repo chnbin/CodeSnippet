7754d1265a26222df71bbef8abcb5887dfff593a
chnbin <chnbin@gmail.com>
2020-05-26 20:47:21 +0800

    Update 1447. Simplified Fractions

--- Leetcode/1447. Simplified Fractions.java
+++ Leetcode/1447. Simplified Fractions.java
@@ -1,3 +1,29 @@
-public class 1447. Simplified Fractions {
+class Solution {
+  public List<String> simplifiedFractions(int n) {
+      List<String> res = new ArrayList<>();
+     
+      for (int i = 2; i <= n; i++) {
+          for (int j = 1; j <= i - 1; j++) {
+              int gcd = findGCD(i, j);
+              String candicate = "";
+              
+              if (gcd == 1) {
+                  candicate = j + "/" + i;
+                  res.add(candicate);
+              }
+          }
+      }
+      
+      return res;
+  }
   
+  private int findGCD(int m, int n) {
+      if (m < n) {
+          return findGCD(n, m);
+      }
+      
+      if (m % n == 0) { return n; }
+      
+      return findGCD(n, m % n);
+  }
 }
