c4d4dd4cdfc4edc397c2a01720facd4ab0918bf9
Chen-Pin Chang <chnbin@gmail.com>
2020-07-11 17:18:13 +0800

    Update 078. Subsets.java
    改成bit全探索

--- Leetcode/078. Subsets.java
+++ Leetcode/078. Subsets.java
@@ -2,28 +2,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
-  public List<List<Integer>> subsets(int[] nums) {
-      List<List<Integer>> ans = new ArrayList<List<Integer>>();
-      List<Integer> currAns = new ArrayList<Integer>();
-      ans.add(new ArrayList<Integer>());
-      subsetsHelper(nums, 0, nums.length, ans, currAns);
-      return ans;
-  }
-  
-  public void subsetsHelper(int[] nums, int start, int end, List<List<Integer>> ans, List<Integer> currAns) {
-      if (start == end) {
-          return ;
-      } else {
-          for(int i = start; i < end; i++) {
-              currAns.add(nums[i]);
-              List<Integer> tmp = new ArrayList<Integer>();
-              for(int num: currAns) {
-                  tmp.add(num);
-              }
-              ans.add(tmp);
-              subsetsHelper(nums, i + 1, end, ans, currAns);
-              currAns.remove(currAns.size() - 1);
-          }
-      }
-  }
+    public List<List<Integer>> subsets(int[] nums) {
+        int total = (int)Math.pow(2.0, nums.length);
+        List<List<Integer>> res = new ArrayList<>();
+        
+        for (int i = 0; i < total; i++) {
+            List<Integer> sol = new ArrayList<>();
+            int curr = i;
+            for (int j = 0; j < 32; j++) {
+                if (((curr >> j) & 1) == 1) {
+                    sol.add(nums[j]);
+                }
+            }
+            res.add(sol);
+        }
+        
+        return res;
+    }
 }
