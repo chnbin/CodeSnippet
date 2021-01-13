ec642fe615b24b204be996199060f03200cd449c
Chen-Pin Chang <chnbin@gmail.com>
2021-01-13 17:22:58 +0800

    Create 881. Boats to Save People.java

---
+++ Leetcode/881. Boats to Save People.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        int res = 0;
+        int left = 0;
+        int right = people.length - 1;
+        
+        Arrays.sort(people);
+        
+        while (left <= right) {
+            if ((people[left] + people[right]) <= limit) {
+                left++;
+            }
+            right--;
+            res++;
+        }
+        
+        return res;
+    }
+}
